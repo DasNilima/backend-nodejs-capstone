4770f82b23f4c65167e73310121385667a83f100
DasNilima <nilima_sust@yahoo.com>
2025-11-14 11:19:12 -0800

    update API for Search Service

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,40 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
+// Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
+        const collection = db.collection(process.env.MONGO_COLLECTION);
         // Initialize the query object
         let query = {};
-        //Task 2: Add the name filter ,Use the following inside the if condition, Task 3: Add other filters and for each filter use the code
 
+        // Add the name filter to the query if the name parameter is not empty
         if (req.query.name && req.query.name.trim() !== '') {
-            query.name = { $regex: req.query.name, $options: "i" };
+            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
 
-        if (req.query.category && req.query.category.trim() !== '') {
+        // Add other filters to the query
+        if (req.query.category) {
             query.category = req.query.category;
         }
-
-        if (req.query.condition && req.query.condition.trim() !== '') {
+        if (req.query.condition) {
             query.condition = req.query.condition;
         }
-
-        if (req.query.age_years && !isNaN(req.query.age_years)) {
-            query.age_years = Number(req.query.age_years);
+        if (req.query.age_years) {
+            query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        const results = await collection.find(query).toArray();
-        res.json(results);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
+    } catch (e) {
+        next(e);
     }
 });
-
 
 module.exports = router;
